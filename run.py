70569d3dd2d9dcbc00ed577555e81aae8253fa7d
Keith Zhang <keithohno@gmail.com>
2020-06-05 13:58:30 -0400

    Integrated With redis-loader
    The master python script has been updated to work with the changes to
    redis-loader. It is currently configured to run experiments on the
    effects of data scarcity on compressibility.

--- run.py
+++ run.py
@@ -3,15 +3,32 @@ import subprocess
 import json
 
 
+class RecParams:
+    def __init__(self, fcount=0, fcap=100, fvolmin=0, fvolmax=400):
+        self.fcount = fcount
+        self.fcap = fcap
+        self.fvolmin = fvolmin
+        self.fvolmax = fvolmax
+
+    def __str__(self):
+        return ("{} {} {} {}".format(
+            self.fcount,
+            self.fcap,
+            self.fvolmin,
+            self.fvolmax))
+
+
 class WLParams:
-    def __init__(self, recct=0, opct=0):
+    def __init__(self, recct=0, opct=0, rprms=RecParams()):
         self.recct = recct
         self.opct = opct
+        self.rprms = rprms
 
-    def to_str(self, recct=False, opct=False, rdprop=False, udprop=False):
-        return("{} {}".format(
+    def to_str(self, recct=False, opct=False, rprms=False):
+        return("{} {} {}".format(
             self.recct if recct else '',
-            self.opct if opct else ''))
+            self.opct if opct else '',
+            str(self.rprms) if rprms else ''))
 
 
 def blockproc(proc):
@@ -30,7 +47,13 @@ def workload(name):
 def load_params(params):
     config = json.dumps({
         "record_count": params.recct,
-        "operation_count": params.opct
+        "operation_count": params.opct,
+        "record_params": {
+            "field_count": params.rprms.fcount,
+            "field_cap": params.rprms.fcap,
+            "field_vol_min": params.rprms.fvolmin,
+            "field_vol_max": params.rprms.fvolmax
+        }
     })
     f = open('redis-loader/config.json', 'w+')
     f.write(config)
@@ -51,7 +74,7 @@ def analyze(name, folder, params):
     for l in lines[1:]:
         ratio = round(int(l.split()[0]) / orig, 5)
         f = open("{}/{}".format(folder, l.split()[1].split('.')[0]), 'a+')
-        f.write("{} {} {}\n".format(params.to_str(opct=True), ratio, orig))
+        f.write("{} {} {}\n".format(params.to_str(opct=True), 1/ratio, orig))
     blockproc(proc)
 
 
@@ -67,13 +90,15 @@ def cleanup(name, folder):
 
 def main():
     sizes = ['256', '512', '1K', '2K', '4K']
-    base = 'DELETE_'
+    base = 'test1'
     core = 'core'
-    for recct in range(20000, 40001, 20000):
-        folder = "{}{:02d}".format(base, int(recct))
+    recct = 4000000
+    for (fmin, fmax) in [(0, 1), (0, 26), (25, 51), (50, 76), (75, 101), (100, 101)]:
+        folder = "test/{}/{:03d}-{:03d}".format(base, fmin, fmax - 1)
         prep(folder)
-        for opct in range(20000, 40001, 20000):
-            params = WLParams(recct=recct, opct=opct)
+        for opct in range(0, 16000001, 500000):
+            rprms = RecParams(fcount=5, fvolmin=fmin, fvolmax=fmax)
+            params = WLParams(recct=recct, opct=opct, rprms=rprms)
             load_params(params)
             workload(core)
             compress(core, folder, sizes)
