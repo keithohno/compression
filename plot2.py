0852583df3bce8f32264292c5605712691a47106
Keith Zhang <keithohno@gmail.com>
2020-06-19 11:04:29 -0400

    Improved Test 2 Plotting Script

--- plot2.py
+++ plot2.py
@@ -6,7 +6,7 @@ import glob
 
 
 def main():
-    plot_all()
+    plot_all('zstd', ['c', 'n', 'p', 'u'], resid=False)
 
 
 def extract(line):
@@ -18,43 +18,46 @@ def csize_to_num(csize):
     return float(csize.replace('K', '000'))
 
 
-def dir_to_num(dir_):
-    return int(dir_.split('_cons')[1].split('-')[0])
+def dir_filter(prefixes):
+    return lambda dir_: dir_.split('/')[-1][0] in prefixes
 
 
-def plot_all():
+def plot_all(ctype, prefixes, resid=True):
     dirs = glob.glob("test/test2/*")
-    # dirs.sort(key=dir_to_num)
+    dirs = list(filter(dir_filter(prefixes), dirs))
+    dirs.sort()
     csizes = os.listdir("{}/lz4/".format(dirs[0]))
     csizes.sort(key=csize_to_num)
-    fig, axs = plt.subplots(2, len(csizes), sharey=True)
-    colors = ['#FF5533', '#DD5555', '#BB5577',
-              '#995599', '#7755BB', '#5555DD', '#3355FF']
-
+    fig, axs = plt.subplots(1, len(csizes), sharey=True)
+    colors = [(.5 * i / len(dirs), i / len(dirs), i / len(dirs))
+              for i in range(len(dirs))]
     for i, csize in enumerate(csizes):
-        for j, ctype in enumerate(['lz4', 'zstd']):
+        xs = []
+        ys = []
+        for k, dir_ in enumerate(dirs):
+            f = open("{}/{}/{}".format(dir_, ctype, csize), 'r')
+            data = list(map(extract, f.readlines()))
+            for d in data:
+                xs.append(d[4])
+                ys.append(d[2])
+        m, b = np.polyfit(xs, ys, 1)
+        for j, dir_ in enumerate(dirs):
+            f = open("{}/{}/{}".format(dir_, ctype, csize), 'r')
+            data = list(map(extract, f.readlines()))
             xs = []
             ys = []
-            for k, dir_ in enumerate(dirs):
-                f = open("{}/{}/{}".format(dir_, ctype, csize), 'r')
-                data = list(map(extract, f.readlines()))
-                for d in data:
-                    xs.append(d[4])
-                    ys.append(d[1])
-            m, b = np.polyfit(xs, ys, 1)
-            for k, dir_ in enumerate(dirs):
-                f = open("{}/{}/{}".format(dir_, ctype, csize), 'r')
-                data = list(map(extract, f.readlines()))
-                xs = []
-                ys = []
-                for d in data:
-                    xs.append(d[4])
-                    y_pred = m * d[3] + b
-                    # ys.append(d[1] - y_pred)
+            for d in data:
+                xs.append(d[4])
+                y_pred = m * d[4] + b
+                if resid:
+                    ys.append(d[2] - y_pred)
+                else:
                     ys.append(d[2])
-                xs = np.array(xs)
-                axs[j][i].plot(xs, ys, '-', color=colors[k], ms=1)
-            axs[j][i].set_title("{}".format(csize))
+            xs = np.array(xs)
+            axs[i].plot(xs, ys, '.', color=colors[j], ms=2)
+        # if not resid:
+        #     axs[i].plot(xs, m*xs + b)
+        axs[i].set_title("{}".format(csize))
 
     for i, dir_ in enumerate(dirs):
         print("{} - {}", colors[i], dir_)
